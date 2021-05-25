7dfb42aa583fb0392afff9212385cb87ed3bcd32
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-25 23:02:17 +0530

    Th object create

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'three'
+import * as THREE from 'three';
 
 // Setup
 
@@ -18,3 +18,24 @@ camera.position.setZ(30);
 camera.position.setX(-3);
 
 renderer.render(scene, camera);
+
+
+// Torus
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({ color: 0xff6347, wireframe: true });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+animate();
